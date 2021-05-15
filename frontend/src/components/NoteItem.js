6d5a2b6256863b395273bf6cc549b05c4def31a2
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-05-15 15:30:08 +0200

    use className insead of class

--- frontend/src/components/NoteItem.js
+++ frontend/src/components/NoteItem.js
@@ -1,7 +1,7 @@
 const NoteItem = ({ note, onToggle }) => {
   const label = note.important ? 'make not important' : 'make important'
   return (
-    <li class="note">
+    <li className="note">
       {note.content}
       <button onClick={() => onToggle(note.id)}>{label}</button>
     </li>
